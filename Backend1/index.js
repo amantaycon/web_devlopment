3bffc0c65fe69cd6b9377b7cf8d4c893f9115d52
aman kumar <amantaycon@gmail.com>
2025-12-02 10:54:03 +0530

    mongoDB screen shot

--- Backend1/index.js
+++ Backend1/index.js
@@ -5,20 +5,27 @@ const path = require("path");
 const public = path.join(__dirname, "/public");
 
 app.use(express.static(public));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use((req, res, next) => {
-    if(req.query.name === "admin"){
-        next();
-    }
-})
+// app.use((req, res, next) => {
+//     if(req.query.name === "admin"){
+//         next();
+//     }
+// })
 
-app.get("/:user/:pass", (req, res) => {
+app.post("/login", (req, res) => {
+    // for quries use like this
     // const username = req.query.user;
     // const password = req.query.pass;
-    const username = req.params.user;
-    const password = req.params.pass;
+
+    // for url parameter use like this
+    // const username = req.params.user;
+    // const password = req.params.pass;
+
+    // for body you use like this
+    const username = req.body.user;
+    const password = req.body.pass;
 
     if (username === "kiet" && password === "mca") {
         res.status(200).json({ id: Date.now(), message: "user verified", date: Date() });
@@ -38,5 +45,5 @@ app.listen(3000, (error) => {
     if (error) {
         console.log(error);
     }
-    console.log("server running on port 3000")
+    console.log("server running on port http://localhost:3000")
 })
